3283beef83022bfaf9bd1c2d94618c3621fd24b9
tbwebb22 <tbwebb22@gmail.com>
2023-11-22 18:47:16 -0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,9 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    fontFamily: {
+      'sans': ['eurostile', 'ui-sans-serif', 'system-ui'],
+    }
   },
   plugins: [],
 }
