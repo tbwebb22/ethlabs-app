ca4e4ab55778da339682319cd1c6f625bed4d009
tbwebb22 <tbwebb22@gmail.com>
2023-11-22 18:47:14 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,15 @@
 import Image from 'next/image'
 
 export default function Home() {
-  // return (
-  //   <main className="flex min-h-screen flex-col items-center justify-between p-24">
-  //     Welcome to ETHLabs
-  //   </main>
-  // )
 
   return (
-    <div className="relative h-[1000px] w-full overflow-hidden">
-            {/* Hero Image */}
+    <div className="absolute top-0 h-[1000px] w-full overflow-hidden z-0">
             <Image 
                 src="/city.png"
                 alt="Hero Image" 
                 layout="fill" 
-                objectFit="cover" // Adjust how the image fits within the container
-                priority // Loads image first for better UX
+                objectFit="cover"
+                priority
             />
             <div className="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-80" />
     </div>
