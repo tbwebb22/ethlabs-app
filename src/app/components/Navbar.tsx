4febab181405f9070d6b382361bfba8c4232fc4c
tbwebb22 <tbwebb22@gmail.com>
2023-11-25 12:22:11 -0500

    Remove commented out code

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,9 +8,7 @@ export default function Navbar() {
 
   return (
     <div className="absolute z-40 w-full">
-    {/* <div className="bg-[#000000] bg-opacity-0 text-white p-3"> */}
     <div className="bg-gradient-to-b from-[#000000] to-transparent p-3">
-    {/* <div className="bg-[#09111c] bg-opacity-80 text-white p-3"> */}
       <div className="container mx-auto flex items-center justify-between">
         <div>
           <Link href="/">
@@ -31,22 +29,16 @@ export default function Navbar() {
           </button>
         </div>
         <div className={`md:flex md:items-center ${isOpen ? 'block' : 'hidden'}`}>
-          {/* <Link className="mx-8 text-xl text-[#aac5e3] hover:text-[#cdffb8]" href="/library">
-            Library
-          </Link> */}
-          <Link className="mx-8 text-3xl font-medium text-[#aac5e3] hover:text-[#cdffb8]" href="/library">
+          <Link className="mx-8 text-3xl font-normal text-[#aac5e3] hover:text-[#cdffb8]" href="/library">
             Library
           </Link>
-          <Link className="mx-8 text-3xl font-medium text-[#aac5e3] hover:text-[#cdffb8]" href="/university">
+          <Link className="mx-8 text-3xl font-normal text-[#aac5e3] hover:text-[#cdffb8]" href="/university">
             University
           </Link>
-          {/* <Link className="rounded-sm px-2 py-1 border-2 border-[#aac5e3] mx-8 text-2xl font-medium text-[#aac5e3] hover:text-[#cdffb8]" href="/university">
-            Call to Action
-          </Link> */}
         </div>
       </div>
     </div>
-    {/* <div className="bg-[#000000] h-[1px]" /> */}
+
     </div>
   );
 };
