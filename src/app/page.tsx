3176741875008468599b3d7bc38827b6a5487280
Taylor Webb <tbwebb22@gmail.com>
2023-12-16 14:18:52 -0500

    Add new city image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ export default function Home() {
 
   return (
     <div className="absolute top-0 h-[1000px] w-full overflow-hidden z-0">
-            <Image 
-                src="/city.png"
-                alt="Hero Image" 
-                layout="fill" 
-                objectFit="cover"
-                priority
-            />
-            <div className="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-80" />
+        <Image 
+            src="/city2.png"
+            alt="Hero Image" 
+            layout="fill" 
+            objectFit="cover"
+            priority
+        />
+        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-80" />
     </div>
   )
 }
