d012289bd6779afb519299b1a519557c812b8c01
Taylor Webb <tbwebb22@gmail.com>
2024-01-20 12:52:30 -0500

    Update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import type { Metadata } from 'next'
+import Head from 'next/head';
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar'
@@ -6,21 +8,40 @@ import Navbar from './components/Navbar'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'ETHLabs',
-  description: 'The ETHLabs webapp',
+  title: 'ETHLabs.io',
+  description: 'The ETHLabs.io webapp',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <html lang="en">
+//       <body className="font-sans">
+//         <Navbar />
+//         {children}
+//       </body>
+//     </html>
+//   )
+// }
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        <title>ETHLabs</title>
+        <meta name="description" content="The ETHLabs webapp" />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700&display=swap" 
+          rel="stylesheet"
+        />
+      </Head>
       <body className="font-sans">
         <Navbar />
         {children}
       </body>
     </html>
-  )
+  );
 }
