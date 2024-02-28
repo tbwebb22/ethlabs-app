823ef006e1556d772afee3a77697ecf4ec3f372f
Taylor Webb <tbwebb22@gmail.com>
2024-02-27 20:12:13 -0500

    Mobile navbar modal

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,11 +3,48 @@ import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+function MobileNavModal({
+  setIsOpen,
+}: {
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  return (
+    <>
+      <dialog className="fixed left-0 top-20 w-full h-full bg-black bg-opacity-50 z-50 overflow-auto backdrop-blur flex justify-center ">
+        <div className="flex flex-col items-start">
+          <Link
+            className="mx-2 px-10 mt-10 text-2xl hover:bg-black/50 font-orbitron font-medium text-[#417ec3] hover:text-[#cdffb8]"
+            href="/blog"
+            onClick={() => setIsOpen(false)}
+          >
+            Blog
+          </Link>
+          <Link
+            className="mx-2 px-10 mt-8 text-2xl hover:bg-black/50 font-orbitron font-medium text-[#417ec3] hover:text-[#cdffb8]"
+            href="/blog"
+            onClick={() => setIsOpen(false)}
+          >
+            Memos
+          </Link>
+          <Link
+            className="mx-2 px-10 mt-8 text-2xl hover:bg-black/50 font-orbitron font-medium text-[#417ec3] hover:text-[#cdffb8]"
+            href="/about"
+            onClick={() => setIsOpen(false)}
+          >
+            About
+          </Link>
+        </div>
+      </dialog>
+    </>
+  );
+}
+
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="absolute z-40 w-full">
+      {isOpen && <MobileNavModal setIsOpen={setIsOpen} />}
       <div className="p-3">
         <div className="mx-auto flex items-center justify-between">
           <div>
@@ -24,9 +61,9 @@ export default function Navbar() {
           <div className="md:hidden">
             <button onClick={() => setIsOpen(!isOpen)}>
               <svg
-                className="w-6 h-6"
+                className="w-12 h-12"
                 fill="none"
-                stroke="currentColor"
+                stroke="#417ec3"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
               >
@@ -39,9 +76,7 @@ export default function Navbar() {
               </svg>
             </button>
           </div>
-          <div
-            className={`md:flex md:items-center ${isOpen ? "block" : "hidden"}`}
-          >
+          <div className={`md:flex md:items-center hidden`}>
             <Link
               className="mx-2 px-10 py-1 text-2xl hover:bg-black/50 font-orbitron font-medium text-[#417ec3] hover:text-[#cdffb8]"
               href="/blog"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    // Use 'min-h-screen' to ensure the grid covers the full viewport height
-    // 'grid grid-rows-3' creates a grid with 3 rows
     <div className="flex flex-grow grid grid-rows-3 ">
-      {/* Empty div for the first row to push the content down */}
-      <div></div>
+      <div />
 
-      {/* Center content in the middle row */}
       <div className="flex justify-center items-center">
         <div className="text-center text-3xl text-[#bdfea3] opacity-70">
           Building and educating on
@@ -18,22 +14,24 @@ export default function Home() {
         </div>
       </div>
 
-      {/* CTA button in the bottom row */}
       <div className="flex justify-center items-center mx-4">
-        <Link href="https://google.com" target="_blank" className="relative">
-          <Image
-            src="/CTAButton2.png"
-            alt="Let's work together"
-            width={400}
-            height={200}
-            priority
-          />
-          <div className="absolute inset-0 flex justify-center items-center">
-            <span className="text-white text-opacity-80 hover:text-[#bdfea3] hover:text-opacity-100 font-orbitron text-2xl font-medium">
-              Lets build together
-            </span>
-          </div>
-        </Link>
+        <div className="group">
+          <Link href="https://google.com" target="_blank" className="relative">
+            <Image
+              src="/CTAButton2.png"
+              className="opacity-80 group-hover:opacity-100"
+              alt="Let's work together"
+              width={400}
+              height={200}
+              priority
+            />
+            <div className="absolute inset-0 flex justify-center items-center">
+              <span className="text-[#bdfea3] text-opacity-80 group-hover:text-[#bdfea3] group-hover:text-opacity-100 font-orbitron text-2xl font-medium">
+                Let&apos;s build together
+              </span>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
