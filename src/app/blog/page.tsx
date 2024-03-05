1e322e7fea52af4139e49d669c5825dee5cd313a
Taylor Webb <tbwebb22@gmail.com>
2024-03-04 19:24:10 -0500

    Responsive blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 
 export default function Blog() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <Link href="/blog/delegatecall">
-        <div className="flex flex-row w-[720px] mt-12 h-18 rounded-r-full text-[#bdfea3] text-opacity-70 hover:text-opacity-90 text-2xl pl-5 pt-[10px] pb-[3px] bg-black/70 hover:bg-black/100">
-          &gt; A mental model for understanding delegatecall
-        </div>
-      </Link>
-    </main>
+    <div className="flex flex-col items-center mt-24">
+      <div className="flex flex-col">
+        <Link href="/blog/delegatecall" className="my-4">
+          <div className="flex flex-row ml-4 mr-4 pr-16 h-18 rounded-r-full text-[#bdfea3] text-opacity-70 hover:text-opacity-90 text-lg md:text-2xl pl-5 pt-[10px] pb-[3px] bg-black/70 ">
+            &gt; A mental model for understanding delegatecall
+          </div>
+        </Link>
+      </div>
+    </div>
   );
 }
